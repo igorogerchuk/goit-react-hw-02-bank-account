b2c4df8ac7964673333e5ac118ec99baaeccfdcc
Igor <igor.ogerchuk@gmail.com>
2019-11-01 01:05:24 +0200

    added balance

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
-
-const TransactionType = {
-  DEPOSIT: 'deposit',
-  WITHDRAW: 'withdraw',
-};
+import React, { Component } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default class Controls extends Component {
   state = {
-    amount: '',
+    amount: ""
   };
 
   handleInput = e => {
@@ -15,13 +12,23 @@ export default class Controls extends Component {
   };
 
   handleDepositButton = () => {
-    this.props.handleClick(this.state.amount, TransactionType.DEPOSIT);
-    this.setState({ amount: '' });
+    const { amount } = this.state;
+    if (amount <= 0) {
+      toast.error("Введите сумму для проведения операции!");
+      return;
+    }
+    this.props.onDeposit(amount);
+    this.setState({ amount: "" });
   };
 
   handleWithdrawButton = () => {
-    this.props.onWithdraw(this.state.amount, TransactionType.WITHDRAW);
-    this.setState({ amount: '' });
+    const { amount } = this.state;
+    if (amount <= 0) {
+      toast.error("Введите сумму для проведения операции!");
+      return;
+    }
+    this.props.onWithdraw(this.state.amount);
+    this.setState({ amount: "" });
   };
 
   render() {
@@ -40,6 +47,17 @@ export default class Controls extends Component {
         <button type="button" onClick={this.handleWithdrawButton}>
           Withdraw
         </button>
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnVisibilityChange
+          draggable
+          pauseOnHover
+        />
       </section>
     );
   }
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,18 @@
-import React, { Component } from 'react';
-import styles from './Dashboard.module.css';
-import Controls from '../Controls';
-const uuidv4 = require('uuid/v4');
+import React, { Component } from "react";
+import styles from "./Dashboard.module.css";
+import Controls from "../Controls";
+import { toast } from "react-toastify";
+const uuidv4 = require("uuid/v4");
+
+const TransactionType = {
+  DEPOSIT: "deposit",
+  WITHDRAW: "withdraw"
+};
 
 export default class Dashboard extends Component {
   state = {
     transactions: [],
-    balance: 0,
+    balance: 0
   };
 
   addTransaction = (amount, type) => {
@@ -14,41 +20,63 @@ export default class Dashboard extends Component {
       id: uuidv4(),
       type,
       amount,
-      date: new Date().toLocaleString(),
+      date: new Date().toLocaleString()
     };
 
     this.setState(state => ({
-      transactions: [...state.transactions, transaction],
+      transactions: [...state.transactions, transaction]
     }));
   };
 
-  // handleWithdraw = amount => {
-  //   const transaction = {
-  //     id: 2,
-  //     type: 'withdraw',
-  //     amount,
-  //     date: new Date().toLocaleString(),
-  //   };
+  handleDeposit = amount => {
+    this.addTransaction(amount, TransactionType.DEPOSIT);
+    this.setState(state => ({
+      balance: state.balance + Number(amount)
+    }));
+  };
 
-  //   this.setState(state => ({
-  //     transactions: [...state.transactions, transaction],
-  //   }));
-  // };
+  handleWithdraw = amount => {
+    const { balance } = this.state;
+    if (amount > balance) {
+      toast.error("На счету недостаточно средств для проведения операции!");
+      return;
+    }
+    this.addTransaction(amount, TransactionType.WITHDRAW);
+    this.setState(state => ({
+      balance: state.balance - Number(amount)
+    }));
+  };
+
+  count = () => {
+    const sums = this.state.transactions.reduce(
+      (acc, transaction) => {
+        return {
+          ...acc,
+          [transaction.type]: acc[transaction.type] + Number(transaction.amount)
+        };
+      },
+      {
+        deposit: 0,
+        withdraw: 0
+      }
+    );
+    console.log(sums);
+    return sums;
+  };
 
   render() {
     return (
       <div class="dashboard">
         {/* <!-- Разметка компонента <Controls> --> */}
         <Controls
-          handleClick={this.addTransaction}
-          // onDeposit={this.handleDeposit}
-          // onWithdraw={this.handleWithdraw}
+          onDeposit={this.handleDeposit}
+          onWithdraw={this.handleWithdraw}
         />
         {/* <!-- Разметка компонента <Balance> --> */}
         <section class="balance">
-          <span>⬆️2000$</span>
-          <span>⬇️1000$</span>
-          <span>Balance: 5000$</span>
+          <span>⬆️{this.count().deposit}$</span>
+          <span>⬇️{this.count().withdraw}$</span>
+          <span>Balance: {this.state.balance}$</span>
         </section>
 
         {/* <!-- Разметка компонента <TransactionHistory> --> */}
