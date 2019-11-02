9a5f705034df8e1df11eaf598a65a816afd497ac
Igor <igor.ogerchuk@gmail.com>
2019-11-02 19:41:40 +0200

    build folder added

---
+++ build/precache-manifest.ffb86d58ecbd9dc591f28759fba0750e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c49822ec3171868360b2bd2f16b9cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28ad4ef7b22448a16932",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "1ec17d1ad949c7ff7a7a",
+    "url": "/static/css/main.e5f5be4c.chunk.css"
+  },
+  {
+    "revision": "28ad4ef7b22448a16932",
+    "url": "/static/js/2.643be242.chunk.js"
+  },
+  {
+    "revision": "1ec17d1ad949c7ff7a7a",
+    "url": "/static/js/main.b5215d2a.chunk.js"
+  },
+  {
+    "revision": "95191575c9c1a0df9b74",
+    "url": "/static/js/runtime-main.d4e071dc.js"
+  }
+]);
